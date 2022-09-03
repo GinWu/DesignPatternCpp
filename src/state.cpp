78f05b074097e1da26248da98e778b5ddcb279d5
gin_wu <995091242@qq.com>
2022-09-04 01:03:23 +0800

    add state pattern

---
+++ src/state.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+class Context;
+
+class State {
+public:
+    virtual void doAction(Context* context) = 0;
+    virtual string toString() = 0;
+    virtual ~State() {};
+};
+
+class Context {
+public:
+    Context() {
+        _state = nullptr;
+    }
+
+    State* getState() {
+        return _state;
+        
+    }
+
+    void setState(State* state) {
+        _state = state;
+    }
+
+    ~Context() {
+        if (_state != nullptr) {
+            delete _state;
+            _state = nullptr;
+        }  
+    }
+private:
+    State* _state;
+};
+
+class StartState: public State {
+public:
+    void doAction(Context* context) override {
+        cout << "This is StartState. " << endl;
+        context->setState(this);
+    }
+
+    string toString() override {
+        return "Start State.";
+    }
+
+};
+
+class StopState: public State {
+public:
+    void doAction(Context* context) override {
+        cout << "This is StopState. " << endl;
+        context->setState(this);
+    }
+
+    string toString() override {
+        return "Stop State.";
+    }
+};
+
+int main() {
+    Context context;
+    StartState startState;
+    startState.doAction(&context);
+    cout << context.getState()->toString() << endl;
+
+    StopState stopState;
+    stopState.doAction(&context);
+    cout << context.getState()->toString() << endl;
+
+    system("pause");
+    return 0;
+}
